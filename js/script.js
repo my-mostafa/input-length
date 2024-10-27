f3236cc81d7f482c913b75515ff0070377edf8e8
mostafa <immostafaeh@gmail.com>
2024-10-27 18:38:39 +0330

    input length

---
+++ js/script.js
@@ -0,0 +1,6 @@
+﻿const tt = document.querySelector("#tt")
+const counter = document.querySelector(".counter")
+const fin = tt.getAttribute("maxlength")
+tt.addEventListener("keyup", function(){
+    counter.innerHTML = fin - tt.value.length
+})
